51295ad806b283af2b34de3fb3be53a152f02635
vivianle <vivianle@umich.edu>
2023-12-03 21:39:53 -0500

    add d3 chart

--- daily_reflection_app/App.js
+++ daily_reflection_app/App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
+import Svg, { G, Path, Text as SvgText, TSpan } from 'react-native-svg';
+import * as d3 from 'd3';
 
-export default function App() {
+const D3Chart = ({ data, width }) => {
+  useEffect(() => {
+    const height = Math.min(width, 500);
+    const radius = Math.min(width, height) / 2;
+
+    const arc = d3.arc()
+      .innerRadius(radius * 0.67)
+      .outerRadius(radius - 1);
+
+    const pie = d3.pie()
+      .padAngle(1 / radius)
+      .sort(null)
+      .value(d => d.value);
+
+    const color = d3.scaleOrdinal()
+      .domain(data.map(d => d.name))
+      .range(d3.quantize(t => d3.interpolateSpectral(t * 0.8 + 0.1), data.length).reverse());
+
+    // Create an SVG element using react-native-svg components
+    const paths = pie(data).map((d, index) => (
+      <Path
+        key={index}
+        fill={color(d.data.name)}
+        d={arc(d)}
+      />
+    ));
+
+    const textElements = pie(data).map((d, index) => (
+      <SvgText
+        key={index}
+        transform={`translate(${arc.centroid(d)})`}
+      >
+        <TSpan dy="-0.4em" fontWeight="bold">{d.data.name}</TSpan>
+        {(d.endAngle - d.startAngle) > 0.25 && (
+          <TSpan x={0} dy="0.7em" fillOpacity={0.7}>
+            {d.data.value.toLocaleString("en-US")}
+          </TSpan>
+        )}
+      </SvgText>
+    ));
+
+    // Return the SVG component
+    return (
+      <Svg width={width} height={height}>
+        <G>{paths}</G>
+        <G fontFamily="sans-serif" fontSize={12} textAnchor="middle">{textElements}</G>
+      </Svg>
+    );
+
+  }, [data, width]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {/* Your chart will be rendered here */}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default D3Chart;
