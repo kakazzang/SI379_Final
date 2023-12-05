f9bcb425f0a71f921b5ef2fa8151454a3d54075a
OwenXu27 <xu-jianxuan@hotmail.com>
2023-12-05 15:12:58 -0500

    update

--- daily_reflection_app/App.js
+++ daily_reflection_app/App.js
@@ -1,35 +1,47 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
 const CalendarScreen = () => {
+  // Placeholder data for November 30th
   const [selectedDate, setSelectedDate] = useState('');
-
-  // Sample data for notes
-  const notes = {
-    '2023-11-30': 'Meeting with team at 10 AM. Discuss project roadmap.'
-    // Add more notes here if needed
-  };
+  const [notes, setNotes] = useState({
+    '2023-11-30': 'This is a placeholder note for November 30th.'
+  });
 
   // Function to handle date selection
   const onDayPress = (day) => {
     setSelectedDate(day.dateString);
   };
 
+  // Function to display the selected date and its note
+  const renderNote = () => {
+    if (!selectedDate) return <Text>Select a date to see notes</Text>;
+
+    const noteForDate = notes[selectedDate];
+    return (
+      <View>
+        <Text style={styles.dateText}>Selected Date: {selectedDate}</Text>
+        <Text style={styles.noteText}>
+          {noteForDate ? noteForDate : 'No note for this date'}
+        </Text>
+      </View>
+    );
+  };
+
+  // Generate marked dates for the calendar
+  const markedDates = Object.keys(notes).reduce((acc, date) => {
+    acc[date] = { marked: true };
+    return acc;
+  }, {});
+
   return (
     <View style={styles.container}>
-      <StatusBar translucent backgroundColor="transparent" />
       <Calendar
         onDayPress={onDayPress}
-        markedDates={{
-          '2023-11-30': { marked: true }
-        }}
+        markedDates={markedDates}
       />
-      {selectedDate && notes[selectedDate] && (
-        <View style={styles.noteView}>
-          <Text style={styles.noteText}>{notes[selectedDate]}</Text>
-        </View>
-      )}
+      {renderNote()}
     </View>
   );
 };
@@ -38,17 +50,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    padding: 20,
+    padding: 40,
     justifyContent: 'center',
   },
-  noteView: {
+  dateText: {
+    fontSize: 18,
+    fontWeight: 'bold',
     marginTop: 20,
-    padding: 10,
-    backgroundColor: '#f0f0f0',
-    borderRadius: 5,
   },
   noteText: {
     fontSize: 16,
+    marginTop: 10,
+    marginBottom: 20,
   }
 });
 
