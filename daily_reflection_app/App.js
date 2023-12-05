f4ff82795b21b00bfa3d177d8efc4ddb7d63483b
vivianle <vivianle@umich.edu>
2023-12-05 18:14:22 -0500

    fixed

--- daily_reflection_app/App.js
+++ daily_reflection_app/App.js
@@ -4,37 +4,37 @@ import MonthlySummaryChart from './Monthly';
 // import D3Chart from './D3Chart';
 
 const App = () => {
-  const data = [
-    { month: '일월', value: 8 },
-    { month: '이월', value: 7 },
-    { month: '삼월', value: 9 },
-    { month: '사월', value: 5 },
-    { month: '오월', value: 6 },
-    { month: '유월', value: 8 },
-    { month: '칠월', value: 7 },
-    { month: '팔월', value: 9 },
-    { month: '구월', value: 8 },
-    { month: '십월', value: 6 },
-    { month: '십일월', value: 7 },
-    { month: '십이월', value: 9 },
-    // Add more as needed
-  ];
-
-  // const data1 = [
-  //   { month: 'January', value: 8 },
-  //   { month: 'February', value: 7 },
-  //   { month: 'March', value: 9 },
-  //   { month: 'April', value: 5 },
-  //   { month: 'May', value: 6 },
-  //   { month: 'June', value: 8 },
-  //   { month: 'July', value: 7 },
-  //   { month: 'August', value: 9 },
-  //   { month: 'September', value: 8 },
-  //   { month: 'October', value: 6 },
-  //   { month: 'November', value: 7 },
-  //   { month: 'December', value: 9 },
+  // const data = [
+  //   { month: '일월', value: 8 },
+  //   { month: '이월', value: 7 },
+  //   { month: '삼월', value: 9 },
+  //   { month: '사월', value: 5 },
+  //   { month: '오월', value: 6 },
+  //   { month: '유월', value: 8 },
+  //   { month: '칠월', value: 7 },
+  //   { month: '팔월', value: 9 },
+  //   { month: '구월', value: 8 },
+  //   { month: '십월', value: 6 },
+  //   { month: '십일월', value: 7 },
+  //   { month: '십이월', value: 9 },
   //   // Add more as needed
   // ];
+
+  const data1 = [
+    { month: 'January', value: 8 },
+    { month: 'February', value: 7 },
+    { month: 'March', value: 9 },
+    { month: 'April', value: 5 },
+    { month: 'May', value: 6 },
+    { month: 'June', value: 8 },
+    { month: 'July', value: 7 },
+    { month: 'August', value: 9 },
+    { month: 'September', value: 8 },
+    { month: 'October', value: 6 },
+    { month: 'November', value: 7 },
+    { month: 'December', value: 9 },
+    // Add more as needed
+  ];
 
   return (
     <View style={styles.container}>
