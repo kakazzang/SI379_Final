57c220b211af68c084d8d431e3ab4904d8935aae
vivianle <vivianle@umich.edu>
2023-12-06 20:52:45 -0500

    sort of

--- daily_reflection_app/App.js
+++ daily_reflection_app/App.js
@@ -1,45 +1,27 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import MonthlySummaryChart from './Monthly';
+// import MonthlySummaryChart from './Monthly';
+import Linec from './Linec';
+// import Line1 from './Line1';
 // import D3Chart from './D3chart.js'
 
 const App = () => {
-  // const data = [
-  //   { month: '일월', value: 8 },
-  //   { month: '이월', value: 7 },
-  //   { month: '삼월', value: 9 },
-  //   { month: '사월', value: 5 },
-  //   { month: '오월', value: 6 },
-  //   { month: '유월', value: 8 },
-  //   { month: '칠월', value: 7 },
-  //   { month: '팔월', value: 9 },
-  //   { month: '구월', value: 8 },
-  //   { month: '십월', value: 6 },
-  //   { month: '십일월', value: 7 },
-  //   { month: '십이월', value: 9 },
-  //   // Add more as needed
-  // ];
-
   const data = [
-    { month: 'Jan', value: 8 },
-    { month: 'Feb', value: 7 },
-    { month: 'Mar', value: 9 },
-    { month: 'Apr', value: 5 },
-    { month: 'May', value: 6 },
-    { month: 'Jun', value: 8 },
-    { month: 'Jul', value: 7 },
-    { month: 'Aug', value: 9 },
-    { month: 'Sept', value: 8 },
-    { month: 'Oct', value: 6 },
-    { month: 'Nov', value: 7 },
-    { month: 'Dec', value: 9 },
-    // Add more as needed
+  { date: "2023-11-28", regretLevel: 1 },
+  { date: "2023-11-29", regretLevel: 2 },
+  { date: "2023-11-30", regretLevel: 4 },
+  { date: "2023-12-01", regretLevel: 3 },
+  { date: "2023-12-02", regretLevel: 3 },
+  { date: "2023-12-03", regretLevel: 5 },
+  { date: "2023-12-04", regretLevel: 2 },
+  { date: "2023-12-05", regretLevel: 3 },
   ];
-
+  
   return (
     <View style={styles.container}>
       {/* <D3Chart data={data} width={300}/> */}
-      <MonthlySummaryChart data={data} width={300}/>
+      {/* <MonthlySummaryChart data={data} width={300}/> */}
+      <Linec data={data} />
     </View>
   );
 };
@@ -49,7 +31,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: '10',
+    // marginBottom: '10',
   },
 });
 
