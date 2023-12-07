1996e948488ef6c2a30da6e058c9a6b2f17c4a4c
vivianle <vivianle@umich.edu>
2023-12-07 01:35:54 -0500

    data.js

--- daily_reflection_app/data.js
+++ daily_reflection_app/data.js
@@ -1,80 +1,198 @@
 
-import React from 'react';
-import { View, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native';
+import { Icon } from 'react-native-elements';
 import MonthlySummaryChart from './monthly';
-// import SummaryNavigation from './summarynav';
+import * as SQLite from 'expo-sqlite';
+import { startOfWeek, endOfWeek, addWeeks, format } from 'date-fns';
 
 const DataScreen = () => {
-  const data = [
-    { date: "2023-11-28", regretLevel: 1 },
-    { date: "2023-11-29", regretLevel: 2 },
-    { date: "2023-11-30", regretLevel: 4 },
-    { date: "2023-12-01", regretLevel: 3 },
-    { date: "2023-12-02", regretLevel: 3 },
-    { date: "2023-12-03", regretLevel: 5 },
-    { date: "2023-12-04", regretLevel: 2 },
-    { date: "2023-12-05", regretLevel: 3 },
-  ];
-  
-  // const db = SQLite.openDatabase('data.db');
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [currentWeek, setCurrentWeek] = useState(new Date());
 
-  // // Function to fetch notes from the database
-  // const fetchNotesFromDatabase = () => {
-  //   db.transaction((tx) => {
-  //     tx.executeSql(
-  //       'SELECT date, userAnswer, regretLevel FROM data WHERE userAnswer IS NOT NULL;',
-  //       [],
-  //       (tx, results) => {
-  //         const newNotes = {};
+  // Open or create the database
+  const db = SQLite.openDatabase('data.db');
 
-  //         for (let i = 0; i < results.rows.length; i++) {
-  //           const row = results.rows.item(i);
-  //           const formattedDate = format(new Date(row.date), 'yyyy-MM-dd');
-  //           newNotes[formattedDate] = {
-  //             userAnswer: row.userAnswer,
-  //             regretLevel: row.regretLevel,
-  //           };
-  //         }
+  // Function to fetch data from the database
+  const fetchDataFromDatabase = () => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT date, regretLevel FROM data;',
+        [],
+        (tx, results) => {
+          const newData = [];
 
-  //         setNotes(newNotes);
-  //       },
-  //       (tx, error) => {
-  //         console.error('Error fetching notes:', error);
-  //       }
-  //     );
-  //   });
-  // };
+          for (let i = 0; i < results.rows.length; i++) {
+            const row = results.rows.item(i);
+            newData.push({
+              date: row.date,
+              regretLevel: row.regretLevel,
+            });
+          }
 
-  // useFocusEffect(
-  //   useCallback(() => {
-  //     fetchNotesFromDatabase();
-  //   }, [])
-  // );
+          setData(newData);
+          setLoading(false);
+        },
+        (tx, error) => {
+          console.error('Error fetching data:', error);
+        }
+      );
+    });
+  };
 
-  // const formattedData = data.map(({ date, regretLevel }) => ({
-  //   date: new Date(date),
-  //   regretLevel,
-  // }));
+  useEffect(() => {
+    fetchDataFromDatabase();
+  }, []);
+
+  const filterDataForWeek = (data, weekStartDate) => {
+    const weekEndDate = endOfWeek(weekStartDate);
+    return data.filter(item => {
+      const itemDate = new Date(item.date);
+      return itemDate >= weekStartDate && itemDate <= weekEndDate;
+    });
+  };
+
+  const changeWeek = (numberOfWeeks) => {
+    setCurrentWeek(addWeeks(currentWeek, numberOfWeeks));
+  };
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  const weeklyData = filterDataForWeek(data, startOfWeek(currentWeek));
 
   return (
     <View style={styles.container}>
-      {/* <D3Chart data={data} width={300}/> */}
-      {/* <MonthlySummaryChart data={data} width={300}/> */}
-      {/* <StatusBar translucent backgroundColor="transparent" /> */}
-      <MonthlySummaryChart data={data} />
-      {/* <SummaryNavigation /> */}
+      <View style={styles.navigationContainer}>
+        <Icon
+          name="chevron-left"
+          type="feather"
+          onPress={() => changeWeek(-1)}
+          size={30}
+          color="#3BB0E5"
+        />
+        <Text style={styles.weekText}>Week of {format(startOfWeek(currentWeek), 'MMM do')}</Text>
+        <Icon
+          name="chevron-right"
+          type="feather"
+          onPress={() => changeWeek(1)}
+          size={30}
+          color="#3BB0E5"
+        />
+      </View>
+      <View style={styles.chart}>
+      <MonthlySummaryChart data={weeklyData} />
+      </View>
     </View>
 )
 
 }
 
 const styles = StyleSheet.create({
+  chart:{
+    flex:1,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    // marginBottom: '10',
+    backgroundColor: 'white',
+  },
+  weekText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  navigationContainer: {
+    marginTop:60,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: 20,
   },
 });
 
 export default DataScreen;
+
+// import React from 'react';
+// import { View, StyleSheet, StatusBar } from 'react-native';
+// import MonthlySummaryChart from './Monthly';
+// // import SummaryNavigation from './summarynav';
+
+// const DataScreen = () => {
+//   // const data = [
+//   //   { date: "2023-11-28", regretLevel: 1 },
+//   //   { date: "2023-11-29", regretLevel: 2 },
+//   //   { date: "2023-11-30", regretLevel: 4 },
+//   //   { date: "2023-12-01", regretLevel: 3 },
+//   //   { date: "2023-12-02", regretLevel: 3 },
+//   //   { date: "2023-12-03", regretLevel: 5 },
+//   //   { date: "2023-12-04", regretLevel: 2 },
+//   //   { date: "2023-12-05", regretLevel: 3 },
+//   // ];
+  
+//   // const db = SQLite.openDatabase('data.db');
+
+//   // // Function to fetch notes from the database
+//   // const fetchNotesFromDatabase = () => {
+//   //   db.transaction((tx) => {
+//   //     tx.executeSql(
+//   //       'SELECT date, userAnswer, regretLevel FROM data WHERE userAnswer IS NOT NULL;',
+//   //       [],
+//   //       (tx, results) => {
+//   //         const newNotes = {};
+
+//   //         for (let i = 0; i < results.rows.length; i++) {
+//   //           const row = results.rows.item(i);
+//   //           const formattedDate = format(new Date(row.date), 'yyyy-MM-dd');
+//   //           newNotes[formattedDate] = {
+//   //             userAnswer: row.userAnswer,
+//   //             regretLevel: row.regretLevel,
+//   //           };
+//   //         }
+
+//   //         setNotes(newNotes);
+//   //       },
+//   //       (tx, error) => {
+//   //         console.error('Error fetching notes:', error);
+//   //       }
+//   //     );
+//   //   });
+//   // };
+
+//   // useFocusEffect(
+//   //   useCallback(() => {
+//   //     fetchNotesFromDatabase();
+//   //   }, [])
+//   // );
+
+//   // const formattedData = data.map(({ date, regretLevel }) => ({
+//   //   date: new Date(date),
+//   //   regretLevel,
+//   // }));
+
+//   return (
+//     <View style={styles.container}>
+//       {/* <D3Chart data={data} width={300}/> */}
+//       {/* <MonthlySummaryChart data={data} width={300}/> */}
+//       {/* <StatusBar translucent backgroundColor="transparent" /> */}
+//       <MonthlySummaryChart data={data} />
+//       {/* <SummaryNavigation /> */}
+//     </View>
+// )
+
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     // marginBottom: '10',
+//   },
+// });
+
+// export default DataScreen;
